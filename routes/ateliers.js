44c5480d1087f87de2047a90a47f159bc0d71794
nturpin <nova.t974@gmail.com>
2018-07-11 11:22:11 +0400

    fonction pour telecharger img ok

--- routes/ateliers.js
+++ routes/ateliers.js
@@ -1,7 +1,43 @@
 var express = require('express');
 var router = express.Router();
-
+var path = require('path');
+var multer = require('multer');
 var atelier = require("../controllers/AtelierController");
+
+// fonction pour l'ajout image
+var storage = multer.diskStorage({
+    destination: function(req, file, cb){
+      cb(null,__dirname + "/../public/img/")
+    },
+    filename: function(req, file, cb){
+      cb(null,file.originalname);
+    }
+  });
+  
+  // Init Upload
+  var upload = multer({
+    storage: storage,
+    limits:{fileSize: 1000000},
+    fileFilter: function(req, file, cb){
+      checkFileType(file, cb);
+    }
+  });
+  
+  // Check File Type
+  function checkFileType(file, cb){
+    // Allowed ext
+    var filetypes = /jpeg|jpg|png|gif/;
+    // Check ext
+    var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    var mimetype = filetypes.test(file.mimetype);
+  
+    if(mimetype && extname){
+      return cb(null,true);
+    } else {
+      cb('Error: Images Only!');
+    }
+  }
 
 //recuperer les ateliers
 // router.get("/", atelier.list);
@@ -13,7 +49,7 @@ router.get("/admin", atelier.listAdmins);
 router.get("/create", atelier.create);
 
 //sauvegarder un atelier. /!\ cest un POST 
-router.post("/save", atelier.save);
+router.post("/save",upload.single('image2'), atelier.save);
 
 //editer une atelier
 router.get("/edit/:id", atelier.edit);
@@ -26,7 +62,7 @@ router.get("/show/:id", atelier.show);
 
 
 //edit update.  /!\ cest un POST 
-router.post("/update/:id", atelier.update);
+router.post("/update/:id",upload.single('image2'), atelier.update);
 
 
 module.exports = router;
